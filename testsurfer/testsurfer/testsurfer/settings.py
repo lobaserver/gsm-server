146d8fbd3bb2d6888799415b0bf09fe06762a5cf
rschnitz <rschnitz@gitlab.tubit.tu-berlin.de>
2017-06-21 15:43:34 +0200

    hallo art

--- testsurfer/testsurfer/testsurfer/settings.py
+++ testsurfer/testsurfer/testsurfer/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'))
+#STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'))
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
--- testsurfer/testsurfer/testsurfer/wsgi.py
+++ testsurfer/testsurfer/testsurfer/wsgi.py
@@ -13,9 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsurfer.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
