523adc6041922bb810de05139fb205aadb80fcc1
rschnitz <rschnitz@gitlab.tubit.tu-berlin.de>
2017-06-21 13:04:26 +0200

    yo

--- testsurfer/testsurfer/responder/models.py
+++ testsurfer/testsurfer/responder/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Content(models.Model):
     message=models.CharField(max_length=144)
+
+
--- testsurfer/testsurfer/testsurfer/settings.py
+++ testsurfer/testsurfer/testsurfer/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'))
+
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
