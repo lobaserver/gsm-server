abf3b9d3491afedb08687fec534653b854fb9777
artpelling <artpelling@gitlab.tubit.tu-berlin.de>
2017-06-27 17:13:43 +0200

    added new json fields

--- testsurfer/testsurfer/responder/models.py
+++ testsurfer/testsurfer/responder/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 class Content(models.Model):
     message=models.CharField(max_length=144)
+    humidity=models.FloatField(default=-100.0)
+    capacity=models.FloatField(default=-100.0)
+    temperature=models.FloatField(default=-100.0)
+    timestamp=models.FloatField(default=-100.0)
 
 
